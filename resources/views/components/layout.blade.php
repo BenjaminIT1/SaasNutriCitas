3be439c7cf8c18010237ac0151d8ae50148dddb5
BenjaminIT1 <a3522110179@alumno.uttehuacan.edu.mx>
2025-06-15 14:28:00 -0600

    Agregado Google Analytics

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,6 +9,16 @@
     <!-- Bootstrap CSS -->
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
+
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-NJMPPTQJS0"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-NJMPPTQJS0');
+</script>
 </head>
 <body>
 
